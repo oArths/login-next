7140354121d38b878cbbdfbc334ca84f070a1551
Arthur <arthurferreira1417@gmail.com>
2024-08-13 17:20:24 -0300

    Feat: fazendo com que a imagem seja carregada com prioridade e de forma otimizada

--- src/components/HomeImage/index.jsx
+++ src/components/HomeImage/index.jsx
@@ -1,11 +1,15 @@
-export default  function HomeImage (){
-
-return(
+import Image from "next/image";
+export default function HomeImage() {
+  return (
     <div className=" h-screen w-3/4 py-5  max-md:hidden ">
-        <img
-          className="w-full h-full object-contain shrink  "
-          src="/images/gradient.svg"
-        />
-      </div>
-)
+      <Image
+        className="w-full h-full object-contain shrink  "
+        src="/images/gradient.svg"
+        alt="imgem de uma mulher"
+        priority
+        width={600} 
+        height={400} 
+      />
+    </div>
+  );
 }
