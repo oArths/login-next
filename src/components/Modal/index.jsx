d533ea49dca80fe599e6810a3dae93f6b90af7f7
Arthur <arthurferreira1417@gmail.com>
2024-08-13 17:18:09 -0300

    Feat: adicionado o metodo createPortal na modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 export default function Modal({
   isOpen,
   onclickConfirm,
@@ -6,13 +9,14 @@ export default function Modal({
   type,
   AltText
 }) {
+  
   function upperFirstLetter(string) {
     if (!string) return 'Usuario'; 
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   if (isOpen) {
-    return (
-      <main className=" flex items-center justify-center absolute top-0 w-screen h-screen  bg-black/[.7] backdrop-blur-sm ">
+    return ReactDOM.createPortal(
+      <main className=" flex items-center justify-center absolute  top-0 w-screen h-screen  bg-black/[.7] backdrop-blur-sm ">
         <div  className=" flex flex-col items-center justify-around  w-96 h-2/5 bg-white rounded-md ">
           <header className="flex flex-col items-center justify-center w-full h-2/5 p-3 gap-2">
             <div className={`flex  ${type ? "" : " rounded-full border-4 border-lime-600 "}`}>{Icon}</div>
@@ -23,7 +27,7 @@ export default function Modal({
 
           <button className={`w-2/3 h-10 text-white rounded-lg  ${type ? "bg-red-600":"bg-lime-600"}`} onClick={onclickConfirm}>Confirmar</button>
         </div>
-      </main>
+      </main>,document.getElementById('modal')
     );
   }
 }
